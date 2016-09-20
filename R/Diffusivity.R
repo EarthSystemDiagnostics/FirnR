9c8a48a108a6fe83f9ae13d4274cabdb3c8203db
Thom Laepple <tlaepple@awi.de>
2016-09-20 10:09:31 +0200

    Diffusionlength with mbar, no dz, on depth

--- R/Diffusivity.R
+++ R/Diffusivity.R
@@ -5,14 +5,14 @@
 ##' @title Diffusivity of water isotopes in firn
 ##' @param rho firn density [kg/m^3]
 ##' @param T annual mean temperature [K]
-##' @param P surface pressure [atm]
+##' @param P surface pressure [mBar]
 ##' @param dD dD  - *logical*; if true, the diffusivity for d2H is returned, otherwise for d18O
 ##' @return diffusivity in [cm^2/s]
 ##' @author Thomas Muench modified by Thomas Laepple
 ##' @examples
 ##' T<-(-60:0)
-##' plot(T,Diffusivity(rho=340,T=273.15+T,P=0.65,dD=FALSE),type="l",lwd=2,main="temperature dependence of diffusivity",xlab="T (Celcius)",ylab="diffusivity (cm^2/s)")
-##' lines(T,Diffusivity(rho=340,T=273.15+T,P=0.65,dD=TRUE),col="red",lwd=2)
+##' plot(T,Diffusivity(rho=340,T=273.15+T,P=650,dD=FALSE),type="l",lwd=2,main="temperature dependence of diffusivity",xlab="T (Celcius)",ylab="diffusivity (cm^2/s)")
+##' lines(T,Diffusivity(rho=340,T=273.15+T,P=650,dD=TRUE),col="red",lwd=2)
 ##' legend("topleft",col=c("black","red"),lwd=2,c("d18O","dD"))
 ##' @export
 Diffusivity <- function(rho,T,P,dD=FALSE)
@@ -22,7 +22,7 @@ Diffusivity <- function(rho,T,P,dD=FALSE)
     R=8.314478                                    # Gas constant
     m=18.02e-3                                    # molar weight of H2O
     p=exp(9.5504+3.53*log(T)-5723.265/T-0.0073*T) # saturation vapour pressure #OK
-    P0=1.                                         # standard atmospheric pressure [atm]
+    P0=1013.25                                        # standard atmospheric pressure [mbar]
     rho_i=920.                                    # density of ice [kg/m3]
     b=1.3                                         # tortuosity constant [Johnsen 2000]
 
