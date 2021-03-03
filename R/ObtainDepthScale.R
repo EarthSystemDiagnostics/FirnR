556d0468ce01537911678d3df45752d79eca8f5f
Thomas Münch <tmuench@awi.de>
2021-03-03 18:02:20 +0100

    add function to obtain full depth scale info.

---
+++ R/ObtainDepthScale.R
@@ -0,0 +1,149 @@
+##' Obtain depth profile information
+##'
+##' Obtain a full profile of depth information, i.e. top, midpoint and bottom
+##' depths as well as corresponding layer thicknesses, from a given subset of
+##' these information. The possible subsets are providing a vector of layer
+##' thicknesses, a vector of midpoint depths, or vectors of top and bottom
+##' depths.
+##'
+##' If a vector of layer thicknesses is provided, or a vector of midpoint
+##' depths, for completing the depth information one needs the additional
+##' information of how deep the top of the first layer is actually below the
+##' surface. For a given layer thickness this is straighforward, as top and
+##' bottom depths follow from the cumulative sum of the thicknesses and are then
+##' merely shifted \code{startDepth} units downwards relative to the surface. If
+##' midpoint depths are provided, it is slightly ambiguous. The implementation
+##' here assumes that in such a case one also knows the depth of the top of the
+##' first layer relative to the surface. This information is used to calculate
+##' the thickness of the first layer around the first midpoint depth, with the
+##' consecutive layer thicknesses then being calculated iteratively.
+##'
+##' @param thickness numeric vector of layer thicknesses; if provided,
+##'   corresponding midpoint, top and bottom depth vectors are calculated (any
+##'   actual input of the latter is ignored).
+##' @param depth numeric vector of midpoint depths; used only if no
+##'   \code{thickness} vector is provided. Then corresponding vectors of layer
+##'   thickness, top and bottom depth are calculated (any actual input of
+##'   \code{top} or \code{bottom} is ignored).
+##' @param top numeric vector of top depths; used together with \code{bottom} to
+##'   calculate corresponding midpoint depths and layer thicknesses, so both
+##'   must have the same length. Only used if no \code{thickness} or
+##'   \code{depth} vectors are provided.
+##' @param bottom numeric vector of bottom depths; used together with \code{top}
+##'   to calculate corresponding midpoint depths and layer thicknesses, so both
+##'   must have the same length. Only used if no \code{thickness} or
+##'   \code{depth} vectors are provided.
+##' @param startDepth distance between the surface and the top of the first firn
+##'   or ice layer. Mandatory input in case either \code{thicknesses} or
+##'   \code{depth} is provided (see details). Must be equal to or larger than
+##'   zero. Default is to assume the first layer to be directly at the surface.
+##' @return A data frame of four variables with the complete set of depth
+##'   information of layer thicknesses as well as midpoint, top and bottom
+##'   depths.
+##' @importFrom rlang .data
+##' @importFrom dplyr %>%
+##' @examples
+##'
+##' # given a vector of layer thicknesses; assume top layer is at surface
+##' thickness <- c(1, 3, 6, 2, 4)
+##' ObtainDepthScale(thickness)
+##'
+##' # given a vector of midpoint depths;  assume top layer is at surface
+##' depth <- c(0.5, 2.5, 7, 11, 14)
+##' ObtainDepthScale(depth = depth)
+##'
+##' # given a vector of top and bottom depths
+##' top <- c(0, 1, 4, 10, 12)
+##' bottom <- c(1, 4, 10, 12, 16)
+##' ObtainDepthScale(top = top, bottom = bottom)
+##'
+##' @author Thomas Münch
+##' @export
+ObtainDepthScale <- function(thickness, depth, top, bottom, startDepth = 0) {
+
+  args <- which(
+    c(!missing(thickness), !missing(depth), !missing(top), !missing(bottom)))
+
+  if (!length(args)) {
+    stop("Need at least 1 depth argument for 'ObtainDepthScale'.",
+         call. = FALSE)
+  }
+
+  if (length(args) > 1) {
+
+    if (args[1] == 1) {
+      warning("Using only 'thickness' argument, ",
+              "ignoring other arguments passed.",
+              call. = FALSE)
+    }
+
+    if (args[1] == 2) {
+      warning("Using only 'depth' argument, ",
+              "ignoring other arguments passed.",
+              call. = FALSE)
+    }
+
+  } else {
+
+    if (args[1] == 3) stop("'top' depths provided, need also 'bottom' depths.")
+    if (args[1] == 4) stop("'bottom' depths provided, need also 'top' depths.")
+
+  }
+
+  mode <- c("thickness", "midpoints", "top/bottom")[args[1]]
+
+  if (mode %in% c("midpoints", "top/bottom")) {
+
+    if (!length(startDepth)) {
+      stop("Missing 'startDepth'.", call. = FALSE)
+    }
+
+    if (startDepth < 0 | is.na(startDepth)) {
+      stop("'startDepth' either < 0 or NA.", call. = FALSE)
+    }
+  }
+
+  if (mode == "midpoints") {
+
+    n <- length(depth)
+    thickness <- numeric(length(n))
+
+    if (startDepth >= depth[1]) {
+      stop("Given 'startDepth' >= first midpoint depth, ",
+           "which would yield zero or negative first layer thickness.",
+           call. = FALSE)
+    }
+
+    thickness[1] <- 2 * (depth[1] - startDepth)
+
+    for (i in 2 : n) {
+      thickness[i] <- 2 * (depth[i] - sum(thickness) - startDepth)
+    }
+
+    mode <- "thickness"
+  }
+
+  if (mode == "thickness") {
+
+    profile <- data.frame(thickness = thickness) %>%
+      dplyr::mutate(bottom = startDepth + cumsum(.data$thickness)) %>%
+      dplyr::mutate(top = c(startDepth, .data$bottom[-dplyr::n()])) %>%
+      dplyr::mutate(depth = (.data$top + .data$bottom) / 2) %>%
+      dplyr::select(depth, top, bottom, thickness)
+
+  } else {
+
+    if (length(top) != length(bottom)) {
+      stop("'top' and 'bottom' must have the same length.", call. = FALSE)
+    }
+
+    profile <- data.frame(top = top, bottom = bottom) %>%
+      dplyr::mutate(depth = (.data$top + .data$bottom) / 2) %>%
+      dplyr::mutate(thickness = .data$bottom - .data$top) %>%
+      dplyr::select(depth, top, bottom, thickness)
+
+  }
+
+  return(profile)
+
+}
---
+++ tests/testthat/test-ObtainDepthScale.R
@@ -0,0 +1,90 @@
+test_that("error handling works", {
+
+  msg <- "Need at least 1 depth argument for 'ObtainDepthScale'."
+  expect_error(ObtainDepthScale(), msg)
+
+  thickness <- depth <- top <- bottom <- 1
+
+  msg <- "Using only 'thickness' argument, ignoring other arguments passed."
+  expect_warning(ObtainDepthScale(thickness, depth, top, bottom), msg)
+  expect_warning(ObtainDepthScale(thickness, depth, top), msg)
+  expect_warning(ObtainDepthScale(thickness, depth, bottom = bottom), msg)
+  expect_warning(ObtainDepthScale(thickness, top = top, bottom = bottom), msg)
+  expect_warning(ObtainDepthScale(thickness, depth), msg)
+  expect_warning(ObtainDepthScale(thickness, top = top), msg)
+  expect_warning(ObtainDepthScale(thickness, bottom = bottom), msg)
+
+  msg <- "Using only 'depth' argument, ignoring other arguments passed."
+  expect_warning(ObtainDepthScale(depth = depth, top = top,
+                                   bottom = bottom), msg)
+  expect_warning(ObtainDepthScale(depth = depth, top = top), msg)
+  expect_warning(ObtainDepthScale(depth = depth, bottom = bottom), msg)
+
+  msg <- "'top' depths provided, need also 'bottom' depths."
+  expect_error(ObtainDepthScale(top = top), msg)
+
+  msg <- "'bottom' depths provided, need also 'top' depths."
+  expect_error(ObtainDepthScale(bottom = bottom), msg)
+
+  msg <- "Missing 'startDepth'."
+  expect_error(ObtainDepthScale(depth = depth, startDepth = NULL), msg)
+  expect_error(ObtainDepthScale(top = top, bottom = bottom,
+                                 startDepth = NULL), msg)
+
+  msg <- "'startDepth' either < 0 or NA."
+  expect_error(ObtainDepthScale(depth = depth, startDepth = -123), msg)
+  expect_error(ObtainDepthScale(top = top, bottom = bottom,
+                                 startDepth = -123), msg)
+  expect_error(ObtainDepthScale(depth = depth, startDepth = NA), msg)
+  expect_error(ObtainDepthScale(top = top, bottom = bottom,
+                                 startDepth = NA), msg)
+
+  msg <- "'top' and 'bottom' must have the same length."
+  expect_error(ObtainDepthScale(top = 1, bottom = 1 : 2), msg)
+
+  msg <- paste("Given 'startDepth' >= first midpoint depth,",
+               "which would yield zero or negative first layer thickness.")
+  expect_error(ObtainDepthScale(depth = c(1, 2), startDepth = 5), msg)
+
+})
+
+test_that("depth conversions work", {
+
+  thicknessExpected <- c(1, 3, 6, 2, 4)
+  topExpected       <- c(0, 1, 4, 10, 12)
+  bottomExpected    <- c(1, 4, 10, 12, 16)
+  depthExpected     <- c(0.5, 2.5, 7, 11, 14)
+
+  profileExpected <- data.frame(
+    depth = depthExpected, top = topExpected,
+    bottom = bottomExpected, thickness = thicknessExpected
+  )
+
+  actual <- ObtainDepthScale(thickness = thicknessExpected)
+  expect_equal(actual, profileExpected)
+
+  actual <- ObtainDepthScale(top = topExpected, bottom = bottomExpected)
+  expect_equal(actual, profileExpected)
+
+  actual <- ObtainDepthScale(depth = depthExpected)
+  expect_equal(actual, profileExpected)
+
+  startDepth <- 41.41
+  topExpected       <- c(0, 1, 4, 10, 12) + startDepth
+  bottomExpected    <- c(1, 4, 10, 12, 16) + startDepth
+  depthExpected     <- c(0.5, 2.5, 7, 11, 14) + startDepth
+
+  profileExpected <- data.frame(
+    depth = depthExpected, top = topExpected,
+    bottom = bottomExpected, thickness = thicknessExpected
+  )
+
+  actual <- ObtainDepthScale(thickness = thicknessExpected,
+                              startDepth = startDepth)
+  expect_equal(actual, profileExpected)
+
+  actual <- ObtainDepthScale(depth = depthExpected,
+                              startDepth = startDepth)
+  expect_equal(actual, profileExpected)
+
+})
