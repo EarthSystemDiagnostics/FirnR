b64d7e3e0001c814b819b06447f466e72b182cf7
Thomas Münch <tmuench@awi.de>
2023-10-12 14:17:51 +0200

    add function to advect proxy record.

---
+++ R/AdvectRecord.R
@@ -0,0 +1,66 @@
+#' Move a record downwards in the firn
+#'
+#' Simulate the effect of new snowfall on the depth position of a given proxy
+#' record for a coordinate system fixed at the snow/firn surface by shifting the
+#' record a number of k bins downward, where k is given by the thickness of the
+#' new snow layer (the 'advection' value) together with the depth resolution of
+#' the record. 
+#'
+#' @param record a data frame of a proxy record with components \code{depth} and
+#'   \code{y} holding the depth scale and the proxy values. The depth scale must
+#'   be equidistant.
+#' @param advection numeric value for the advection, i.e. the depth value by
+#'   which the record is moved deeper into the firn, measured in the same
+#'   physical units as component \code{depth} in \code{record}.
+#' @param clip logical; the default method shifts the proxy record out through
+#'   the "bottom" of the record so that the shifted bins are lost. For
+#'   \code{clip = FALSE}, the record is extended by the number of bins shifts so
+#'   that it can still hold all original proxy values; see the examples.
+#' @return a data frame with components \code{depth} and \code{y} holding the
+#'   shifted (advected) proxy record.
+#' @author Thomas Münch
+#' @examples
+#'
+#' # in this example, the lowermost three proxy values are "lost" by the
+#' # downward advection since clip = TRUE:
+#' data.frame(depth = 1 : 10, y = 1 : 10) %>%
+#'   AdvectRecord(advection = 3)
+#'
+#' # for clip = FALSE all proxy values are kept but we get a longer data frame:
+#' data.frame(depth = 1 : 10, y = 1 : 10) %>%
+#'   AdvectRecord(advection = 3, clip = FALSE)
+#' 
+#' @export
+#'
+AdvectRecord <- function(record, advection, clip = TRUE) {
+
+  if (any(is.na(match(c("depth", "y"), colnames(record))))) {
+    stop("Expected column names for 'record' are: 'depth', 'y'.",
+         call. = FALSE)
+  }
+  if (nrow(record) <= 1) stop("Length of proxy record needs to be > 1.")
+  if (length(advection) != 1) stop("'advection' needs to be of length 1.")
+  if (is.na(advection)) stop("Missing value passed for 'advection'.")
+  if (advection < 0) stop("'advection' value needs to be > 0.")
+  if (length(depth.res <- diff(record$depth)) > 1) {
+    if (sd(depth.res) != 0) stop("Require constant depth resolution.")
+  }
+
+  depth.res <- depth.res[1]
+  k <- round(advection / depth.res)
+
+  if (k < 1) stop("'advection' value < depth resolution of record.")
+
+  if (clip) {
+
+    dplyr::mutate(record, y = prxytools::Lag(record$y, shift = k))
+
+  } else {
+
+    data.frame(
+      depth = c(record$depth, max(record$depth) + depth.res * (1 : k)),
+      y = prxytools::Lag(c(record$y, rep(NA, k)), shift = k)
+    )
+
+  }
+}
---
+++ tests/testthat/test-AdvectRecord.R
@@ -0,0 +1,83 @@
+test_that("error handling works", {
+
+  msg <- "Expected column names for 'record' are: 'depth', 'y'."
+  expect_error(AdvectRecord(data.frame(foo = 1, bar = 1), advection = 1),
+               msg, fixed = TRUE)
+  expect_error(AdvectRecord(data.frame(depth = 1, bar = 1), advection = 1),
+                 msg, fixed = TRUE)
+  expect_error(AdvectRecord(data.frame(foo = 1, y = 1), advection = 1),
+               msg, fixed = TRUE)
+
+  msg <- "Length of proxy record needs to be > 1."
+  expect_error(AdvectRecord(data.frame(depth = 1, y = 1), advection = 1),
+               msg, fixed = TRUE)
+
+  msg <- "'advection' needs to be of length 1."
+  expect_error(
+    AdvectRecord(data.frame(depth = 1 : 2, y = 1 : 2), advection = c(1, 3)),
+    msg, fixed = TRUE)
+  expect_no_error(
+    AdvectRecord(data.frame(depth = 1 : 2, y = 1 : 2), advection = c(1, NULL)))
+
+  msg <- "Missing value passed for 'advection'."
+  expect_error(
+    AdvectRecord(data.frame(depth = 1 : 2, y = 1 : 2), advection = NA),
+    msg, fixed = TRUE)
+
+  msg <- "'advection' value needs to be > 0."
+  expect_error(
+    AdvectRecord(data.frame(depth = 1 : 2, y = 1 : 2), advection = -3.7),
+    msg, fixed = TRUE)
+
+  msg <- "Require constant depth resolution."
+  expect_error(
+    AdvectRecord(data.frame(depth = c(1, 4, 5, 9), y = 1 : 4), advection = 1),
+    msg, fixed = TRUE)
+
+  msg <- "'advection' value < depth resolution of record."
+  expect_error(
+    AdvectRecord(data.frame(depth = 1 : 4, y = 1 : 4), advection = 0.5),
+    msg, fixed = TRUE)
+  expect_no_error( # due to internal rounding
+    AdvectRecord(data.frame(depth = seq(1.5, 10.5, 3), y = 1 : 4),
+                 advection = 2))
+
+})
+
+test_that("advection shift works", {
+
+  # with clipping
+
+  actual <- data.frame(depth = 1 : 10, y = 1 : 10) %>%
+    AdvectRecord(advection = 3)
+  expected <- data.frame(depth = 1 : 10, y = c(rep(NA, 3), 1 : 7))
+
+  expect_equal(actual, expected)
+
+  actual <- data.frame(depth = 1 : 10, y = 1 : 10) %>%
+    AdvectRecord(advection = 8.45)
+  expected <- data.frame(depth = 1 : 10, y = c(rep(NA, 8), 1 : 2))
+
+  expect_equal(actual, expected)
+
+  actual <- data.frame(depth = seq(1.5, 22.5, 3), y = 1 : 8) %>%
+    AdvectRecord(advection = 3)
+  expected <- data.frame(depth = seq(1.5, 22.5, 3), y = c(NA, 1 : 7))
+
+  expect_equal(actual, expected)
+
+  # without clipping
+
+  actual <- data.frame(depth = 1 : 10, y = 1 : 10) %>%
+    AdvectRecord(advection = 3, clip = FALSE)
+  expected <- data.frame(depth = 1 : 13, y = c(rep(NA, 3), 1 : 10))
+
+  expect_equal(actual, expected)
+
+  actual <- data.frame(depth = seq(1.5, 22.5, 3), y = 1 : 8) %>%
+    AdvectRecord(advection = 12.47, clip = FALSE)
+  expected <- data.frame(depth = seq(1.5, 34.5, 3), y = c(rep(NA, 4), 1 : 8))
+
+  expect_equal(actual, expected)
+
+})
