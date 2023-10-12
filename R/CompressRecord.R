e5987af923fc9ec13c3e518223807392f9fd8e39
Thomas Münch <tmuench@awi.de>
2023-10-12 16:01:34 +0200

    allow for non-equidistant depth resolutions.

--- R/CompressRecord.R
+++ R/CompressRecord.R
@@ -2,18 +2,18 @@
 #'
 #' This function simulates the effect of firn densification by linearly
 #' interpolating given data onto a compressed (shorter) depth scale. This
-#' compressed depth scale is found by subtracting the amount of compression
-#' (\code{stretch}) from the maximum depth of the original depth scale and
-#' dividing it into equal bins the same number as in the original depth
-#' vector. The proxy values on the compressed depth scale are found by linear
-#' interpolation of the original values from the compressed depth scale onto the
-#' original depth scale.
+#' compressed depth scale is obtained from subtracting the amount of compression
+#' (\code{stretch}) from the length of the original record and
+#' dividing this shorter length into bins with size proportional to the original
+#' record's resolution. The proxy values on the compressed depth scale are found
+#' by linear interpolation of the original values from the compressed depth
+#' scale onto the original depth scale.
 #'
 #' @param record a data frame with components \code{depth} and \code{y} holding
 #'   the original depth scale and proxy values.
 #' @param stretch numeric value of the amount of compression of the original
 #'   depth scale measured in the same physical units as \code{depth} in
-#'   \code{record}.
+#'   \code{record}; cannot be larger than the length of the original record.
 #' @return a data frame with components \code{depth} and \code{y} holding the
 #'   original depth scale and the interpolated (compressed) proxy values.
 #' @author Thomas Münch
@@ -43,14 +43,15 @@ CompressRecord <- function(record, stretch) {
   if (length(stretch) != 1) stop("'stretch' needs to be of length 1.")
   if (is.na(stretch)) stop("Missing value passed for 'stretch'.")
 
-  if (stretch >= (max(record$depth) - record$depth[1])) {
+  if (stretch >= (len <- diff(range(record$depth)))) {
     stop("Compression value >= range (max - min) of original depth scale.")
   }
   if (stretch < 0) warning("Negative 'stretch' parameter yields longer record.")
 
+  # new bin sizes
+  new.bin.s <- diff(record$depth) * (1 - (stretch / len))
   # depth scale after densification
-  new.depth <- seq(record$depth[1], max(record$depth) - stretch,
-                   length.out = nrow(record))
+  new.depth <- c(record$depth[1], record$depth[1] + cumsum(new.bin.s))
 
   # approximate record on original depth scale
   data.frame(
