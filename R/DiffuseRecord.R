9b73e6fe27aee99bd364f0a0b93d47eac2f403db
Thomas Münch <tmuench@awi.de>
2026-06-09 11:48:55 +0200

    document new data frame handling.

--- R/DiffuseRecord.R
+++ R/DiffuseRecord.R
@@ -1,58 +1,60 @@
-#' Diffuse a proxy record.
+#' Diffuse a proxy record
 #'
 #' This function diffuses a proxy time series or record with a given diffusion
 #' length by convolution with a Gaussian kernel.
 #'
 #' The function expects a numeric vector with the depth or time-dependent
-#' diffusion length of the same length as \code{rec}, or a single value to use
+#' diffusion length of the same length as the \code{record}, or a single value to use
 #' a constant diffusion length. The input diffusion length is internally
-#' rescaled according to the resolution of the record given by \code{res}, so
-#' that the diffusion length corresponds to the number of record bins
-#' effectively being smoothed. The convolution integral is then solved by a
-#' simple summation over the kernel width which is set to ~ 10 times the local
-#' diffusion length.
+#' rescaled according to the resolution of the record, so that the diffusion
+#' length corresponds to the number of record bins effectively being
+#' smoothed. It is thus mandatory for the user to ensure that the record's
+#' sampling scale (e.g., depth or time) and the diffusion length are measured in
+#' the same physical units.
+#'
+#' The convolution integral is solved by a simple summation over the kernel
+#' width which is set to ~ 10 times the local diffusion length.
 #'
 #' The parameter \code{pad} controls the behaviour at the ends of the
 #' record. Without "padding" (\code{pad = FALSE}), both ends of the diffused
-#' record will contain \code{NA} values as a result of the kernel extending
-#' beyond the record ends, with the number of \code{NA} values depending on the
-#' local width of the diffusion kernel, thus on the values of the diffusion length
-#' around the upper and lower end of the record. These \code{NA} values can be
-#' avoided by setting \code{pad = TRUE}, which is the default setting. At the
-#' upper end, the kernel is then clipped to the range below the surface; at the
-#' lower end, the record is extended with the mean average value of \code{rec}
-#' to an additional length of ~ 10 times the overall maximum value of
-#' \code{sigma}.
+#' record will contain \code{NA} values as a result of the diffusion kernel
+#' extending beyond the record ends, with the number of \code{NA} values
+#' depending on the local width of the kernel, thus on the values of the
+#' diffusion length around the upper and lower end of the record. These
+#' \code{NA} values can be avoided by setting \code{pad = TRUE} (the default),
+#' which causes the diffusion kernel to be clipped at the upper end to the range
+#' below the surface, and the \code{record} at the lower end to be extended with
+#' its mean value to an additional length of ~ 10 times the overall maximum
+#' value of \code{sigma}.
 #'
-#' @param rec numeric vector of the record to be diffused, tabulated at an
-#'   equidistant resolution given by \code{res}.
+#' @param record a data frame with components \code{depth} and \code{y} holding
+#'   the sampling (e.g., depth or time) scale and the proxy values to be
+#'   diffused. The sampling scale must be equidistant.
 #' @param sigma numeric vector of diffusion lengths; either of the same length
-#'   as \code{rec} to provide local diffusion lengths corresponding to every
-#'   depth or time point at which \code{rec} is tabulated, or of length one to
-#'   diffuse \code{rec} with a constant diffusion length. The diffusion length
-#'   must be given in the same units as is the resolution of \code{rec}.
-#' @param res numeric; single value of the equidistant resolution of \code{rec}
-#'   in the same units as \code{sigma}; e.g., a record resolution of 1 cm
-#'   (\code{res = 1}) requires that the diffusion lengths are also given in
-#'   units of cm, a resolution of 5 years (\code{res = 5}) requires diffusion
-#'   lengths given in units of years. This is needed for rescaling the
-#'   diffusion length into units of bin size; see also Details.
-#' @param pad logical; the default setting \code{TRUE} avoids \code{NA} values
-#'   at the top and bottom of the diffused record, which would otherwise result
-#'   from the finite record length, by clipping the diffusion kernel at the top
-#'   and extending the record with its mean at the bottom; see also Details.
-#' @return numeric vector of the diffused version of \code{rec}.
+#'   as the \code{record} to provide local diffusion lengths corresponding to
+#'   every sampling point of the \code{record}, or of length one to diffuse the
+#'   \code{record} with a constant diffusion length. The diffusion length must
+#'   be given in the same units as the record's sampling scale, see Details.
+#' @param pad logical, defaults to \code{TRUE}; normally, the finite record
+#'   length leads to \code{NA} values at the top and bottom of the diffused
+#'   record. The default setting of \code{pad} avoids this by clipping the
+#'   diffusion kernel at the top of the record and extending the record with its
+#'   mean at the bottom; see also Details.
+#' @return a data frame with the diffused version of the proxy \code{record}.
 #' @author Thomas Münch, with contributions by Thomas Laepple
 #' @examples
 #'
-#' ## Diffuse white noise with a linearly increasing diffusion length
-#' rec <- rnorm(n = 1000)
-#' var.sigma <- seq(1, 8, length.out = 1000)
-#' diffused <- DiffuseRecord(rec = rec, sigma = var.sigma)
-#' plot(rec, type = 'l', las = 1, xlab = "depth in cm", ylab = "data", main = 
-#'      "white noise diffusion with linearly increasing diffusion length")
+#' # Diffuse white noise with a linearly increasing diffusion length
+#' n <- 1000
+#' record <- data.frame(depth = seq(n), y = rnorm(n))
+#' sigma <- seq(1, 8, length.out = n)
+#' diffused <- DiffuseRecord(record, sigma)
+#' plot(record, type = 'l', las = 1,
+#'      xlab = "Depth (a.u.)", ylab = "Data (a.u.)",
+#'      main =
+#'        "White noise diffusion with linearly increasing diffusion length")
 #' lines(diffused, col = "red")
-#' legend('topleft', c("original record", "diffused record"),
+#' legend("topleft", c("Original record", "Diffused record"),
 #'        lty = 1, col = 1 : 2, bty = "n")
 #'
 #' @export
