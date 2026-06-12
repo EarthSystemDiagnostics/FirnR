9ce23947e112035608a7a174441fc3878ebe8fcf
Thomas Münch <tmuench@awi.de>
2026-06-12 13:58:53 +0200

    use single roxygen hash.

--- R/SimProfile.R
+++ R/SimProfile.R
@@ -1,109 +1,109 @@
-##' Simulate virtual firn profile
-##'
-##' Forward-simulate a virtual firn proxy depth profile based on given
-##' temperature and precipitation time series and local climatic parameters; see
-##' Details.
-##'
-##' This function implements the generation of a proxy depth profile measured on
-##' a firn/ice core based on the following governing processes:
-##' linear conversion (calibration) to proxy units, precipitation intermittency,
-##' densification, firn diffusion, and sampling process. In detail this works as
-##' follows:
-##'
-##' Layers of precipitated snow are piled up from an input time series of
-##' precipitation data with thickness in water equivalent units, and are
-##' associated with the time of the precipitation event and the proxy value
-##' in the snow, the latter being based on a climatic input time series and a
-##' linear proxy calibration. The uneven layers are converted to an
-##' unequidistant true depth scale based on a calculated Herron-Langway
-##' densification model. Subsequently, this depth scale is, together with the
-##' corresponding time information and proxy data, interpolated onto an
-##' equidistant high-resolution firn/ice depth scale with a depth resolution
-##' given by the minimum observed layer thickness in real units. If desired, the
-##' proxy depth profile is then diffused with a calculated diffusion length. For
-##' the output, the resulting depth profile is block averaged to a given coarser
-##' resolution mimicking the typical size of sampling intervals upon cutting a
-##' firn/ice core.
-##'
-##' @param time numeric vector of time points corresponding to the observation
-##'   points of the precipitation and temperature time series. Must be in a date
-##'   format which supports calculation of midpoint and average values (via
+#' Simulate virtual firn profile
+#'
+#' Forward-simulate a virtual firn proxy depth profile based on given
+#' temperature and precipitation time series and local climatic parameters; see
+#' Details.
+#'
+#' This function implements the generation of a proxy depth profile measured on
+#' a firn/ice core based on the following governing processes:
+#' linear conversion (calibration) to proxy units, precipitation intermittency,
+#' densification, firn diffusion, and sampling process. In detail this works as
+#' follows:
+#'
+#' Layers of precipitated snow are piled up from an input time series of
+#' precipitation data with thickness in water equivalent units, and are
+#' associated with the time of the precipitation event and the proxy value
+#' in the snow, the latter being based on a climatic input time series and a
+#' linear proxy calibration. The uneven layers are converted to an
+#' unequidistant true depth scale based on a calculated Herron-Langway
+#' densification model. Subsequently, this depth scale is, together with the
+#' corresponding time information and proxy data, interpolated onto an
+#' equidistant high-resolution firn/ice depth scale with a depth resolution
+#' given by the minimum observed layer thickness in real units. If desired, the
+#' proxy depth profile is then diffused with a calculated diffusion length. For
+#' the output, the resulting depth profile is block averaged to a given coarser
+#' resolution mimicking the typical size of sampling intervals upon cutting a
+#' firn/ice core.
+#'
+#' @param time numeric vector of time points corresponding to the observation
+#'   points of the precipitation and temperature time series. Must be in a date
+#'   format which supports calculation of midpoint and average values (via
 ##'   \code{approx()} and \code{mean()}).
-##' @param precip numeric vector with a precipitation time series (in
-##'   w.eq. units) tabulated at the time points in \code{time}.
-##' @param temperature numeric vector with a temperature time series (in deg C)
-##'   tabulated at the time points in \code{time}.
-##' @param data numeric vector with a data time series from which the firn
-##'   proxy profile is to be simulated, tabulated at the time points in
-##'   \code{time}; the default is to use the \code{temperature} time series, but
-##'   also any other suitable environmental proxy can be input here for profile
-##'   simulation. In such case, the \code{temperature} input is still needed but
-##'   only to obtain an average temperature value for the densification rate and
-##'   diffusion length calculations.
-##' @param pressure local atmospheric surface pressure in mbar. Defaults to
-##'   observed average pressure at EDML site.
-##' @param rho.surface local surface density in kg/m^3.
-##' @param accumulation.scale factor to convert the average value of
-##'   \code{precip} into units of mm w.eq. per year. Default is to assume the
-##'   precipitation data is given in units of mm w.eq. per day.
-##' @param depth.scale factor to convert the precipitation data into units of m
-##'   w.eq. Default is to assume the precipitation data is given in mm w.eq.
-##' @param alpha slope of a linear calibration to convert the input \code{data}
-##'   into the desired units of the firn profile; e.g. a linear
-##'   temperature-to-isotope calibration.
-##' @param beta the same as \code{alpha} but providing the intercept of the
-##'   linear calibration.
-##' @param dz.out output resolution in m of the simulated firn profile to
-##'   mimick a typical firn/ice core sampling process. Defaults to 3 cm.
-##' @param diffuse logical to control whether the simulated firn profile
-##'   shall be diffused according to the standard firn diffusion model.
-##' @return A data frame of three variables with the simulated firn profile:
-##'   firn/ice midpoint depths in m (column \code{depth}), time relative to the
-##'   first observation point of the input time series (column \code{time}), and
-##'   corresponding firn profile (proxy) value (column \code{y}). Additionally,
-##'   attributes are attached to the data frame which include information on the
-##'   simulation run: applied linear calibration parameters, used atmospheric
-##'   pressure, surface snow density and output resolution, the diffusion flag,
-##'   and the date of the run.
-##' @examples
-##'
-##' # --- Simple simulation example with constant daily accumulation ---
-##'
-##' # create 5-yr precipitation time series with constant daily amount
-##' # (with annual accumulation amount from EDML site)
-##' nyr <- 5
-##' precip <- rep(70 / 365, times = nyr * 365)
-##'
-##' # create bi-harmonic model of sesasonal cycle in temperature at EDML;
-##' # repeat 5 years and add some noise
-##' seasonal.par <- c(A0 = -44.5, A1 = 13, A2 = 5, phi1 = 10, phi2 = 50)
-##' temperature <- rep(FirnR:::CreateHarmonicSeries(seasonal.par), nyr) +
-##'                rnorm(length(precip), sd = 2)
-##'
-##' # arbitrary time vector
-##' time <- as.Date(-1 * (length(precip) : 1), origin = "2020-01-01")
-##'
-##' # run simulation
-##' profile.nodiff <- SimProfile(time, precip, temperature, diffuse = FALSE)
-##' profile <- SimProfile(time, precip, temperature)
-##'
-##' # compare original time series to simulated time series in the firn
-##' plot(time, temperature, type = "l", ylim = c(-65, -15),
-##'      xlab = "Time", ylab = "Original and simulated profiles (a.u.)")
-##' lines(profile.nodiff$time, profile.nodiff$y, col = 4, lwd = 2)
-##' lines(profile$time, profile$y, col = 2, lwd = 2)
-##' legend("topright",
-##'        c("Original ts", "Simulated ts w/o diffusion",
-##'          "Simulated ts with diffusion"),
-##'        lty = 1, lwd = c(1, 2, 2), col = c(1, 4, 2))
-##'
-##' # show simulated depth profile
-##' plot(profile$depth, profile$y, col = 2, type = "l", lwd = 2,
-##'      xlab = "Depth (m)", ylab = "Simulated depth profile")
-##' abline(h = mean(profile$y), lty = 2)
-##'
-##' @author Thomas Münch
-##' @export
+#' @param precip numeric vector with a precipitation time series (in
+#'   w.eq. units) tabulated at the time points in \code{time}.
+#' @param temperature numeric vector with a temperature time series (in deg C)
+#'   tabulated at the time points in \code{time}.
+#' @param data numeric vector with a data time series from which the firn
+#'   proxy profile is to be simulated, tabulated at the time points in
+#'   \code{time}; the default is to use the \code{temperature} time series, but
+#'   also any other suitable environmental proxy can be input here for profile
+#'   simulation. In such case, the \code{temperature} input is still needed but
+#'   only to obtain an average temperature value for the densification rate and
+#'   diffusion length calculations.
+#' @param pressure local atmospheric surface pressure in mbar. Defaults to
+#'   observed average pressure at EDML site.
+#' @param rho.surface local surface density in kg/m^3.
+#' @param accumulation.scale factor to convert the average value of
+#'   \code{precip} into units of mm w.eq. per year. Default is to assume the
+#'   precipitation data is given in units of mm w.eq. per day.
+#' @param depth.scale factor to convert the precipitation data into units of m
+#'   w.eq. Default is to assume the precipitation data is given in mm w.eq.
+#' @param alpha slope of a linear calibration to convert the input \code{data}
+#'   into the desired units of the firn profile; e.g. a linear
+#'   temperature-to-isotope calibration.
+#' @param beta the same as \code{alpha} but providing the intercept of the
+#'   linear calibration.
+#' @param dz.out output resolution in m of the simulated firn profile to
+#'   mimick a typical firn/ice core sampling process. Defaults to 3 cm.
+#' @param diffuse logical to control whether the simulated firn profile
+#'   shall be diffused according to the standard firn diffusion model.
+#' @return A data frame of three variables with the simulated firn profile:
+#'   firn/ice midpoint depths in m (column \code{depth}), time relative to the
+#'   first observation point of the input time series (column \code{time}), and
+#'   corresponding firn profile (proxy) value (column \code{y}). Additionally,
+#'   attributes are attached to the data frame which include information on the
+#'   simulation run: applied linear calibration parameters, used atmospheric
+#'   pressure, surface snow density and output resolution, the diffusion flag,
+#'   and the date of the run.
+#' @examples
+#'
+#' # --- Simple simulation example with constant daily accumulation ---
+#'
+#' # create 5-yr precipitation time series with constant daily amount
+#' # (with annual accumulation amount from EDML site)
+#' nyr <- 5
+#' precip <- rep(70 / 365, times = nyr * 365)
+#'
+#' # create bi-harmonic model of sesasonal cycle in temperature at EDML;
+#' # repeat 5 years and add some noise
+#' seasonal.par <- c(A0 = -44.5, A1 = 13, A2 = 5, phi1 = 10, phi2 = 50)
+#' temperature <- rep(FirnR:::CreateHarmonicSeries(seasonal.par), nyr) +
+#'                rnorm(length(precip), sd = 2)
+#'
+#' # arbitrary time vector
+#' time <- as.Date(-1 * (length(precip) : 1), origin = "2020-01-01")
+#'
+#' # run simulation
+#' profile.nodiff <- SimProfile(time, precip, temperature, diffuse = FALSE)
+#' profile <- SimProfile(time, precip, temperature)
+#'
+#' # compare original time series to simulated time series in the firn
+#' plot(time, temperature, type = "l", ylim = c(-65, -15),
+#'      xlab = "Time", ylab = "Original and simulated profiles (a.u.)")
+#' lines(profile.nodiff$time, profile.nodiff$y, col = 4, lwd = 2)
+#' lines(profile$time, profile$y, col = 2, lwd = 2)
+#' legend("topright",
+#'        c("Original ts", "Simulated ts w/o diffusion",
+#'          "Simulated ts with diffusion"),
+#'        lty = 1, lwd = c(1, 2, 2), col = c(1, 4, 2))
+#'
+#' # show simulated depth profile
+#' plot(profile$depth, profile$y, col = 2, type = "l", lwd = 2,
+#'      xlab = "Depth (m)", ylab = "Simulated depth profile")
+#' abline(h = mean(profile$y), lty = 2)
+#'
+#' @author Thomas Münch
+#' @export
 SimProfile <- function(time, precip, temperature, data = temperature,
                        pressure = 677, rho.surface = 340,
                        accumulation.scale = 365, depth.scale = 10^-3,
--- R/TemporalDiffusionLength.R
+++ R/TemporalDiffusionLength.R
@@ -1,57 +1,57 @@
-##' Calculate the diffusion length in years
-##'
-##' This function calculates the diffusion length in firn in time units (years)
-##' based on calculating the diffusion length in depth units
-##' (see \code{\link{DiffusionLength}}) and converting it from depth units in
-##' temporal units using the firn density from the Herron-Langway model
-##' (see \code{\link{DensityHL}}).
-##'
-##' The diffusion length can be calculated for several sites with varying
-##' climatic input parameters \code{T}, \code{P}, \code{bdot} and
-##' \code{rho.surface}. Note that for this all input parameter vectors must
-##' either have the same length. Else, length-one vectors are recycled to match
-##' the length of the longest input; if this still results in varying vector
-##' lengths, an error is issued.
-##'
-##' @param core.length the simulated core length in metre for calculating the
-##'   Herron-Langway firn density and the diffusion length. If this length is
-##'   not sufficient to cover the requested time span given by \code{nt} and
-##'   \code{t.res}, diffusion length values for the remaining time points are
-##'   filled with the last properly obtained value. This issues only a warning
-##'   since if the simulated core is long enough to reach the ice, the diffusion
-##'   length is anyway constant, but it is a problem if the simulated core is
-##'   far too shallow.
-##' @param z.res the resolution in metre of the simulated firn core.
-##' @param nt the number of time points for the output temporal diffusion
-##'   length.
-##' @param t.res the temporal resolution for the diffusion length in time units
-##'   [yr]; i.e. the total time span covered is \code{t.res * nt}.
-##' @param T local annual mean surface temperature (10-m firn temperature) in
-##'   [K].
-##' @param P local surface pressure in [mbar].
-##' @param bdot local mass accumulation rate in [kg/m^2/yr].
-##' @param rho.surface local surface firn density in [kg/m^3].
-##' @param dD logical; if \code{TRUE} the diffusion length for deuterium is
-##'   returned, otherwise for oxygen-18. Defaults to \code{FALSE}.
-##' @param JohnsenCorr logical; whether or not to apply the Johnsen correction
-##'   to the Arrhenius rate constants in the Herron-Langway model for central
-##'   Greenland sites.
-##' @param names optional character vector of site names.
-##' @return the temporal diffusion lengths in a data frame of \code{nt} rows and
-##'   a minimum of two columns, where the first column is the time axis and the
-##'   second or other columns are the diffusion lengths for the requested
-##'   site(s).
-##' @seealso \code{\link{DiffusionLength}}, \code{\link{DensityHL}}
-##' @examples
-##' # do the calculation for the default parameters at Kohnen Station:
-##' sigma.years <- TemporalDiffusionLength()
-##'
-##' # mimicing two sites by adjusting one parameter to have length 2
-##' # (all other parameters are then recycled to length 2):
-##' sigma.years <- TemporalDiffusionLength(T = c(-30, -50) + 273.15,
-##'                                        names = c("Site A", "Site B"))
-##' @author Thomas Münch
-##' @export
+#' Calculate the diffusion length in years
+#'
+#' This function calculates the diffusion length in firn in time units (years)
+#' based on calculating the diffusion length in depth units
+#' (see \code{\link{DiffusionLength}}) and converting it from depth units in
+#' temporal units using the firn density from the Herron-Langway model
+#' (see \code{\link{DensityHL}}).
+#'
+#' The diffusion length can be calculated for several sites with varying
+#' climatic input parameters \code{T}, \code{P}, \code{bdot} and
+#' \code{rho.surface}. Note that for this all input parameter vectors must
+#' either have the same length. Else, length-one vectors are recycled to match
+#' the length of the longest input; if this still results in varying vector
+#' lengths, an error is issued.
+#'
+#' @param core.length the simulated core length in metre for calculating the
+#'   Herron-Langway firn density and the diffusion length. If this length is
+#'   not sufficient to cover the requested time span given by \code{nt} and
+#'   \code{t.res}, diffusion length values for the remaining time points are
+#'   filled with the last properly obtained value. This issues only a warning
+#'   since if the simulated core is long enough to reach the ice, the diffusion
+#'   length is anyway constant, but it is a problem if the simulated core is
+#'   far too shallow.
+#' @param z.res the resolution in metre of the simulated firn core.
+#' @param nt the number of time points for the output temporal diffusion
+#'   length.
+#' @param t.res the temporal resolution for the diffusion length in time units
+#'   [yr]; i.e. the total time span covered is \code{t.res * nt}.
+#' @param T local annual mean surface temperature (10-m firn temperature) in
+#'   [K].
+#' @param P local surface pressure in [mbar].
+#' @param bdot local mass accumulation rate in [kg/m^2/yr].
+#' @param rho.surface local surface firn density in [kg/m^3].
+#' @param dD logical; if \code{TRUE} the diffusion length for deuterium is
+#'   returned, otherwise for oxygen-18. Defaults to \code{FALSE}.
+#' @param JohnsenCorr logical; whether or not to apply the Johnsen correction
+#'   to the Arrhenius rate constants in the Herron-Langway model for central
+#'   Greenland sites.
+#' @param names optional character vector of site names.
+#' @return the temporal diffusion lengths in a data frame of \code{nt} rows and
+#'   a minimum of two columns, where the first column is the time axis and the
+#'   second or other columns are the diffusion lengths for the requested
+#'   site(s).
+#' @seealso \code{\link{DiffusionLength}}, \code{\link{DensityHL}}
+#' @examples
+#' # do the calculation for the default parameters at Kohnen Station:
+#' sigma.years <- TemporalDiffusionLength()
+#'
+#' # mimicing two sites by adjusting one parameter to have length 2
+#' # (all other parameters are then recycled to length 2):
+#' sigma.years <- TemporalDiffusionLength(T = c(-30, -50) + 273.15,
+#'                                        names = c("Site A", "Site B"))
+#' @author Thomas Münch
+#' @export
 TemporalDiffusionLength <- function(core.length = 1000, z.res = 0.01,
                                     nt = 2000, t.res = 1,
                                     T = 273.15 - 44.5, P = 677,
